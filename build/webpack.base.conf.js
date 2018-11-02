a1f07ee69adeffbf4bc29a696fbddfbd3e6c3ba7
Evgenii Aleksanyan <alexanyan1292@gmail.com>
2018-11-02 15:12:29 +0300

    Deploy7

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,6 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-require('@babel/polyfill')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -23,7 +22,7 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: ["@babel/polyfill", './src/main.js']
+    app: './src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
